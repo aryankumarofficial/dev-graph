29d593f526233687461edc59c3adaf5d657ce126
Aryan Kumar <aryanak9163@gmail.com>
2026-08-13 23:30:45 +0530

    feat: adds the developer queries for cognoDB

---
+++ src/lib/queries/developers.ts
@@ -0,0 +1,68 @@
+import {DeveloperSkill, DeveloperTechnology, IDeveloper} from "@/types/developer.type";
+import {runQuery} from "@/lib/cognodb";
+
+export async function getDevelopers(): Promise<IDeveloper[]> {
+    return runQuery<IDeveloper>(
+        `
+        MATCH (d:Developer)
+        RETURN
+            d.id AS id,
+            d.name AS name,
+            d.username AS username,
+            d.bio AS bio,
+        ORDER BY d.name
+        `
+    );
+}
+
+export async function getDeveloperById(developerId: IDeveloper): Promise<IDeveloper> {
+    const records = await runQuery<IDeveloper>(
+        `
+        MATCH (d:Developer {id:$developerId})
+        RETURN
+            d.id AS id,
+            d.name AS name,
+            d.username AS username,
+            d.bio AS bio,
+        `,
+        {developerId}
+    )
+    return records[0] ?? null;
+}
+
+export async function getDeveloperSkills(
+    developerId: string,
+): Promise<DeveloperSkill[]> {
+    return runQuery<DeveloperSkill>(`
+        MATCH (d:Developer {id:$developerId})
+            -[:HAS_SKILL]->
+            (s:Skill)
+        RETURN
+            s.id AS id,
+            s.name AS name,
+            s.category AS category,
+        ORDER BY s.name
+    `,
+        {developerId}
+    )
+}
+
+export async function getDeveloperTechnologies(
+    developerId: string,
+): Promise<DeveloperTechnology[]> {
+    return runQuery<DeveloperTechnology>(`
+        MATCH (d:DeveloperTechnology {id:$developerId})
+            -[:HAS_SKILL]->
+            (s:Skill)
+            -[:RELATED_TO]->
+            (t:Technology)
+        RETURN DISTINCT
+            t.id AS id,
+            t.name AS name,
+            t.category AS category,
+            t.description AS description,
+        ORDER BY s.name
+    `,
+        {developerId}
+    )
+}
---
+++ src/types/developer.type.ts
@@ -0,0 +1,19 @@
+export interface IDeveloper {
+    id: string;
+    name: string;
+    username: string;
+    bio: string;
+}
+
+export interface DeveloperSkill {
+    id: string;
+    name: string;
+    category: string;
+}
+
+export interface DeveloperTechnology {
+    id: string;
+    name: string;
+    category: string;
+    description: string;
+}
